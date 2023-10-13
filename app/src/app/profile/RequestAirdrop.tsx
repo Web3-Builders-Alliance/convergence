609fb602da8e0c699ecb1ba0ca5843bab497745f
Thomas Gehrmann <s6thgehr@gmail.com>
2023-10-13 03:04:19 +0200

    Use public rpc for airdrop

--- app/src/app/profile/RequestAirdrop.tsx
+++ app/src/app/profile/RequestAirdrop.tsx
@@ -1,6 +1,11 @@
 import useUserSOLBalanceStore from "@/stores/useUserSOLBalanceStore";
 import { useConnection, useWallet } from "@solana/wallet-adapter-react";
-import { LAMPORTS_PER_SOL, TransactionSignature } from "@solana/web3.js";
+import {
+  Connection,
+  LAMPORTS_PER_SOL,
+  TransactionSignature,
+  clusterApiUrl,
+} from "@solana/web3.js";
 import { FC, useCallback, useState } from "react";
 import toast from "react-hot-toast";
 
@@ -17,12 +22,18 @@ export const RequestAirdrop: FC = () => {
 
       return;
     }
+    const publicConnection = new Connection(clusterApiUrl("devnet"), {
+      commitment: "confirmed",
+    });
 
     let signature: TransactionSignature = "";
 
     setIsLoading(true);
     try {
-      signature = await connection.requestAirdrop(publicKey, LAMPORTS_PER_SOL);
+      signature = await publicConnection.requestAirdrop(
+        publicKey,
+        LAMPORTS_PER_SOL
+      );
 
       // Get the lates block hash to use on our transaction and confirmation
       let latestBlockhash = await connection.getLatestBlockhash();
